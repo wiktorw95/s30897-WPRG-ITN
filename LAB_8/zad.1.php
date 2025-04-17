835566430f5dca8aca5c9632395c9bbddb4d8505
Wiktor Wilk <52862835+wiktorw95@users.noreply.github.com>
2025-04-17 16:59:29 +0200

    Updated a little zad.1

--- LAB_8/zad.1.php
+++ LAB_8/zad.1.php
@@ -12,13 +12,13 @@
 </form>
 <?php
 if(isset($_POST['sent'])) {
-    $input_text = $_POST['text'];
+    $input = $_POST['text'];
 
-    $upper = strtoupper($input_text);
+    $upper = strtoupper($input);
     echo "<p>Uppercasae: $upper</p>";
-    $lower = strtolower($input_text);
+    $lower = strtolower($input);
     echo "<p>Lowercase: $lower</p>";
-    $first_upper = ucfirst($input_text);
+    $first_upper = ucfirst($input);
     echo "<p>First Uppercase: $first_upper</p>";
     $first_upper_of_each_word = ucwords($first_upper);
     echo "<p>First Uppercase: $first_upper_of_each_word</p>";
